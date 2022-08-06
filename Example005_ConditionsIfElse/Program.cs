5b599c17e5228b819dbd6dde8fd00af4107f2e19
Irik <freedomirik@yandex.ru>
2022-08-06 10:50:55 +0500

    :w

--- Example005_ConditionsIfElse/Program.cs
+++ Example005_ConditionsIfElse/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ирик")
+{
+    Console.WriteLine("Здравствуйте, Господин!");
+}
+else
+{
+    Console.Write("Ты кто такой, {0}?", username);
+}
